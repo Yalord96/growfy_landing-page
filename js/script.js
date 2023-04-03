e9e954a282874d8cda31a5839b643afebcf664bb
Sergey Nosenko <yalord96@gmail.com>
2023-04-03 11:10:02 +0300

    prod

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+	const targetItem = e.target;
+	if (targetItem.closest('.icon-menu')) {
+		document.documentElement.classList.toggle('menu-open');
+	}
+}
